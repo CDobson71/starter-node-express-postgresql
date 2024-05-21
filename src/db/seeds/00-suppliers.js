2c5b2a1b7c289ce24ea327628c65d9746d20fd28
Your Name <connor.dobson1@gmail.com>
2024-05-20 21:27:02 -0600

    test

---
+++ src/db/seeds/00-suppliers.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+
+const suppliers = require("../fixtures/suppliers");
+
+
+exports.seed = async function(knex) {
+  return knex
+    .raw("TRUNCATE TABLE suppliers RESTART IDENTITY CASCADE")
+    .then(function () {
+      return knex("suppliers").insert(suppliers);
+    });
+};
